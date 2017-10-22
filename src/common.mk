544a6de9edc677a46c84aa9ae1a09e9a27b28aa2
Andrew Gaffney <andrew@agaffney.org>
2017-10-22 17:40:56 -0500

    generate debug symbols

--- src/common.mk
+++ src/common.mk
@@ -3,11 +3,13 @@ CWD=$(dir $(firstword $(MAKEFILE_LIST)))
 MKFILE_DIRNAME=$(shell basename $(abspath $(dir $(firstword $(MAKEFILE_LIST)))))
 
 CC=gcc
-CFLAGS=-m32 -fno-stack-protector
+CFLAGS=-ggdb -O0 -m32 -fno-stack-protector -nostdinc -Wall -fno-builtin
 LD=ld
 LDFLAGS=-m elf_i386 --oformat elf32-i386
 NASM=nasm
 NASM_FLAGS=-f elf32
+OBJCOPY=objcopy
+STRIP=strip
 
 OUTPUT ?= $(MKFILE_DIRNAME).o
 
