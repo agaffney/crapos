bea93a58cb7a0081a12cc45e799cf22b63db9c92
Andrew Gaffney <andrew@agaffney.org>
2017-11-20 11:40:35 -0700

    add support for specifying object suffix

--- common.mk
+++ common.mk
@@ -1,5 +1,10 @@
+# Generate list of object files in directory tree by finding all source files
+# and appending a suffix to them. This function takes two optional arguments:
+#
+# 1 - path to search in, defaults to current directory
+# 2 - suffix to add, defaults to '.o'
 define generate_obj_list
-	$(patsubst %, %.o, $(shell find $(or $(1),.) -type f -a \( -name '*.c' -o -name '*.asm' -o -name '*.s' -o -name '*.S' \)))
+	$(patsubst %, %$(or $(2),.o), $(shell find $(or $(1),.) -type f -a \( -name '*.c' -o -name '*.asm' -o -name '*.s' -o -name '*.S' \)))
 endef
 
 CC=gcc
