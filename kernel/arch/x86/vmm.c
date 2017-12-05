ef7d9afab21cd514a9b645f6b0cbcabe91dcb35a
Andrew Gaffney <andrew@agaffney.org>
2017-12-04 17:07:19 -0700

    VMM progress

--- kernel/arch/x86/vmm.c
+++ kernel/arch/x86/vmm.c
@@ -1,6 +1,7 @@
 #include <arch/x86/vmm.h>
 
 x86_page_dir * PAGE_DIR;
+void * NEXT_PHYS_PAGE;
 
 void vmm_init() {
 	// Reuse initial page directory from boot.s to conserve memory
@@ -9,9 +10,43 @@ void vmm_init() {
 	PAGE_DIR->page_tables[0] = (x86_page_table *)(((uint32_t)&boot_pagetab1 - KERN_OFFSET) | (PAGE_FLAG_PRESENT + PAGE_FLAG_WRITE));
 	PAGE_DIR->page_tables[768] = PAGE_DIR->page_tables[0];
 	reload_page_dir();
+	// Set the next available physical page to the location of the
+	// _kernel_end symbol
+	NEXT_PHYS_PAGE = &_kernel_end - KERN_OFFSET;
+	void * foo = map_page();
+	char buf[30];
+	sprintf(buf, "page addr = %x", foo);
+	kprint(buf);
+	kprint_newline();
 }
 
 void reload_page_dir() {
 	uint32_t val = (uint32_t)PAGE_DIR - KERN_OFFSET;
 	asm volatile ("movl %0, %%cr3" :: "r"(val));
+}
+
+void * get_next_phys_page() {
+	void * new_page = NEXT_PHYS_PAGE;
+	NEXT_PHYS_PAGE += PAGE_SIZE;
+	return new_page;
+}
+
+void * map_page() {
+	void * new_page = get_next_phys_page();
+	uint16_t page_dir_idx = get_page_dir_index(new_page);
+	uint16_t page_tbl_idx = get_page_table_index(new_page);
+	char buf[20];
+	sprintf(buf, "0x%x, 0x%x", page_dir_idx, page_tbl_idx);
+	kprint(buf);
+	kprint_newline();
+
+	return new_page;
+}
+
+uint16_t get_page_dir_index(void * addr) {
+	return (uint16_t)((uint32_t)addr / (PAGE_SIZE * PAGE_TABLE_SIZE));
+}
+
+uint16_t get_page_table_index(void * addr) {
+	return (uint16_t)((uint32_t)addr / PAGE_SIZE % (PAGE_TABLE_SIZE * PAGE_DIR_SIZE));
 }
--- kernel/include/arch/x86/vmm.h
+++ kernel/include/arch/x86/vmm.h
@@ -5,11 +5,20 @@
 // This should match the value with the same name defined in boot.s
 #define KERN_OFFSET 0xC0000000
 
+#define PAGE_SIZE 4096
+
+#define PAGE_DIR_SIZE 1024
+#define PAGE_DIR_ENTRY_SIZE 4
+#define PAGE_TABLE_SIZE 1024
+#define PAGE_TABLE_ENTRY_SIZE 4
+
 #define PAGE_FLAG_PRESENT 1
 #define PAGE_FLAG_WRITE   2
 
 extern void * boot_pagedir;
 extern void * boot_pagetab1;
+
+extern void _kernel_end;
 
 typedef struct {
 	void * pages[1024];
@@ -21,3 +30,7 @@ typedef struct {
 
 void vmm_init();
 void reload_page_dir();
+void * map_page();
+
+uint16_t get_page_dir_index(void * addr);
+uint16_t get_page_table_index(void * addr);
