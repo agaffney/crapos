fd059809ab693b66be2800a4c48cb2d6af8cf5d5
Andrew Gaffney <andrew@agaffney.org>
2017-12-10 17:49:23 -0600

    add shift/pop/delete functions for linked list

--- libc/data/linked_list.c
+++ libc/data/linked_list.c
@@ -1,5 +1,6 @@
 #include <stddef.h>
 #include <core/kprint.h>
+#include <core/vmm.h>
 #include <data/linked_list.h>
 
 void * Linked_List_get(Linked_List * list, uint32_t index) {
@@ -8,10 +9,10 @@ void * Linked_List_get(Linked_List * list, uint32_t index) {
 	for (count = 0; count < index; count++) {
 		item = item->next;
 	}
-	return item->item;
+	return item->data;
 }
 
-void Linked_List_push(Linked_List * list, void * item) {
+void Linked_List_push(Linked_List * list, void * data) {
 	Linked_List_Item * new_item;
 	// Use pre-allocated empty first item if present, otherwise allocate
 	// memory
@@ -21,11 +22,57 @@ void Linked_List_push(Linked_List * list, void * item) {
 	} else {
 		new_item = kmalloc(sizeof(Linked_List_Item));
 	}
-	kprint("new_item = 0x%x, list->last = 0x%x, list->length = %d\n", new_item, list->last, list->length);
-	new_item->item = item;
+	new_item->data = data;
+	if (list->first == NULL) {
+		list->first = new_item;
+	}
 	if (list->last != NULL) {
 		list->last->next = new_item;
 	}
 	list->last = new_item;
 	list->length++;
+	kprint("new_item = 0x%x, list->last = 0x%x, list->length = %d\n", new_item, list->last, list->length);
+}
+
+void Linked_List_delete(Linked_List * list, size_t idx) {
+	if (idx > (list->length - 1)) {
+		return;
+	}
+	size_t count;
+	Linked_List_Item * item = list->first;
+	Linked_List_Item * prev_item = NULL;
+	for (count = 0; count < idx; count++) {
+		prev_item = item;
+		item = item->next;
+	}
+	if (prev_item != NULL) {
+		prev_item->next = item->next;
+	}
+	if (idx == (list->length - 1)) {
+		list->last = prev_item;
+	}
+	if (idx == 0) {
+		list->first = item->next;
+	}
+	list->length--;
+}
+
+void * Linked_List_shift(Linked_List * list) {
+	void * data = NULL;
+	if (list->length > 0) {
+		kprint("shift(): list->first=0x%x\n", list->first);
+		data = list->first->data;
+		Linked_List_delete(list, 0);
+	}
+	kprint("shift(): data=0x%x, list->length=%d\n", data, list->length);
+	return data;
+}
+
+void * Linked_List_pop(Linked_List * list) {
+	void * data = NULL;
+	if (list->length > 0) {
+		data = list->last->data;
+		Linked_List_delete(list, list->length - 1);
+	}
+	return data;
 }
--- libc/include/data/linked_list.h
+++ libc/include/data/linked_list.h
@@ -18,11 +18,13 @@ typedef struct _Linked_List Linked_List;
 struct _Linked_List_Item {
 	struct _Linked_List_Item * next;
 	Linked_List * head;
-	void * item;
+	void * data;
 };
 
 typedef struct _Linked_List_Item Linked_List_Item;
 
 void * Linked_List_get(Linked_List * list, uint32_t index);
 void Linked_List_push(Linked_List * list, void * item);
-
+void * Linked_List_pop(Linked_List * list);
+void * Linked_List_shift(Linked_List * list);
+void Linked_List_delete(Linked_List * list, size_t idx);
