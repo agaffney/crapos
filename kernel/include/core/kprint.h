d5593143e10f989159cb5289836c4869178706d8
Andrew Gaffney <andrew@agaffney.org>
2017-12-16 21:49:39 -0600

    making the debug output pretty for the wife

--- kernel/include/core/kprint.h
+++ kernel/include/core/kprint.h
@@ -2,4 +2,10 @@
 
 void kprint(const char* restrict format, ...);
 
-#define kdebug(fmt, ...) kprint("[DEBUG] (%s:%d):%s(): " fmt,  __FILE__, __LINE__, __func__, ##__VA_ARGS__)
+#define KDEBUG_RESET_BOLD "\033[0;1m"
+#define KDEBUG_RESET "\033[0m"
+#define KDEBUG_BG_BLUE "\033[44m"
+#define KDEBUG_BG_GREEN "\033[42m"
+#define KDEBUG_BG_MAGENTA "\033[45m"
+
+#define kdebug(fmt, ...) kprint(KDEBUG_RESET_BOLD "[DEBUG] (" KDEBUG_BG_BLUE "%s" KDEBUG_RESET_BOLD ":" KDEBUG_BG_GREEN "%d" KDEBUG_RESET_BOLD "):" KDEBUG_BG_MAGENTA "%s()" KDEBUG_RESET_BOLD ": " KDEBUG_RESET fmt,  __FILE__, __LINE__, __func__, ##__VA_ARGS__)
