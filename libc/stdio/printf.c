56ea478999cdeac8048a2820578afb843dbf9db7
Andrew Gaffney <andrew@agaffney.org>
2018-01-02 14:46:06 -0600

    add flag and field width support to printf

--- libc/stdio/printf.c
+++ libc/stdio/printf.c
@@ -22,10 +22,53 @@ int vsprintf(char *out, const char* restrict format, va_list parameters) {
 	while (*format != '\0') {
 		if (format[0] == '%') {
 			format++;
+			// %% gives '%'
 			if (format[0] == '%') {
 				out[written] = format[0];
 				written++;
-			} else if (format[0] == 'c') {
+				continue;
+			}
+			int alt_form = 0, left_just = 0, prepend_sign = 0;
+			char pad_char = '\0';
+			int field_width = 0;
+			// Look for flags
+			int break_out = 0;
+			while(format[0] != '\0') {
+				switch(format[0]) {
+					case '0':
+						pad_char = '0';
+						break;
+					case ' ':
+						pad_char = ' ';
+						break;
+					case '#':
+						alt_form = 1;
+						break;
+					case '-':
+						left_just = 1;
+						break;
+					case '+':
+						prepend_sign = 1;
+						break;
+					default:
+						break_out = 1;
+						break;
+				}
+				if (break_out) {
+					break;
+				}
+				format++;
+			}
+			// Look for field-width
+			while (format[0] >= '0' && format[0] <= '9') {
+				// Multiply by 10 for each additional digit
+				if (field_width > 0) {
+					field_width = field_width * 10;
+				}
+				field_width += format[0] - '0';
+				format++;
+			}
+			if (format[0] == 'c') {
 				char c = (char) va_arg(parameters, int /* char promotes to int */);
 				out[written] = c;
 				format++;
@@ -40,7 +83,7 @@ int vsprintf(char *out, const char* restrict format, va_list parameters) {
 				format++;
 				written += len;
 			} else if (format[0] == 'd' || format[0] == 'i') {
-				signed int val  = va_arg(parameters, signed int);
+				signed int val = va_arg(parameters, signed int);
 				itoa(val, itoa_buf, 10);
 				size_t len = strlen(itoa_buf);
 				for (i = 0; i < len; i++) {
@@ -49,7 +92,7 @@ int vsprintf(char *out, const char* restrict format, va_list parameters) {
 				format++;
 				written += len;
 			} else if (format[0] == 'u') {
-				unsigned int val  = va_arg(parameters, unsigned int);
+				unsigned int val = va_arg(parameters, unsigned int);
 				itoa(val, itoa_buf, 10);
 				size_t len = strlen(itoa_buf);
 				for (i = 0; i < len; i++) {
@@ -61,16 +104,30 @@ int vsprintf(char *out, const char* restrict format, va_list parameters) {
 				unsigned int val = va_arg(parameters, unsigned int);
 				itoa(val, itoa_buf, 16);
 				size_t len = strlen(itoa_buf);
+				if (alt_form) {
+					out[written++] = '0';
+					out[written++] = 'x';
+				}
+				if (pad_char) {
+					for (i = len; i < field_width; i++) {
+						out[written++] = pad_char;
+					}
+				}
 				for (i = 0; i < len; i++) {
 					out[written + i] = ( format[0] == 'x' ? tolower(itoa_buf[i]) : toupper(itoa_buf[i]) );
 				}
 				format++;
 				written += len;
+			// Non-standard format specifier for binary output
 			} else if (format[0] == 'b') {
-				// Non-standard format specifier for binary output
 				unsigned int val = va_arg(parameters, unsigned int);
 				itoa(val, itoa_buf, 2);
 				size_t len = strlen(itoa_buf);
+				if (pad_char) {
+					for (i = len; i < field_width; i++) {
+						out[written++] = pad_char;
+					}
+				}
 				for (i = 0; i < len; i++) {
 					out[written + i] = itoa_buf[i];
 				}
