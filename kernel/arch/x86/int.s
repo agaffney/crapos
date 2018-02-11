51cfd5ee93ceae73ff0e265c77ac98a05c5674cb
Andrew Gaffney <andrew@agaffney.org>
2018-02-11 17:59:40 -0600

    save/restore more registers when calling interrupt handlers

--- kernel/arch/x86/int.s
+++ kernel/arch/x86/int.s
@@ -1,11 +1,33 @@
+# Macro to save general/segment registers to stack and set DS register to
+# appropriate value for running kernel code
 .macro SAVE_REGS
+	# Push general registers to stack
 	pushal
+	# Push segment registers to stack
+	push %ds
+	push %es
+	push %fs
+	push %gs
+	# Set current data segment to kernel code selector
+	push %ebx
+	mov $0x10, %bx
+	mov %bx, %ds
+	pop %ebx
 .endm
 
+# Macro to restore general/segment registers from stack when returning from
+# interrupt handler
 .macro RESTORE_REGS
+	# Restore segment registers from stack
+	pop %gs
+	pop %fs
+	pop %es
+	pop %ds
+	# Restore general registers from stack
 	popal
 .endm
 
+# Generic interrupt handler macro
 .macro INTERRUPT num
 .global _asm_int_\num
 _asm_int_\num:
